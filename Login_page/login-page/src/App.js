35ec1640ff0a53bb6c2d28b55a455512e7bc0ed9
tanwer-pradeep <pradeepsinghonline@gmail.com>
2021-06-26 12:49:39 +0530

    new Login page app

---
+++ Login_page/login-page/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="login-form">
+      <h3>Email</h3>
+      <input type="text/email" placeholder="Email" value=""></input>
+      
+      <h3>Password</h3>
+      <input type="password" placeholder="Password" value=""></input>
+    </div>
+  );
+}
+
+export default App;
